55ba3080a8a798ee074b8b913d5c0814b4fef00b
jitendravee <jitendravee098@gmail.com>
2023-10-21 10:17:47 +0530

    one

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex flex-cols gap-3 text-3xl font-bold underline py-3 px-3 mt-3 mb-3 bg-blue-300 rounded-xl" >
+    <h1 >
+      Hello world!
+    </h1>
+    <p>helllo</p>
+    </div>
+  )
+}
